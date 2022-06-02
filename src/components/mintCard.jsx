874408af19d31060eba6abeeaaa249f3ad17146a
Daniyal Tayyab <daniyaltayyab91@gmail.com>
2022-06-02 18:34:23 +0500

    mint complete

--- src/components/mintCard.jsx
+++ src/components/mintCard.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import "../css/styles.css";
 
-const MintCard = ({ handleIncrement, handleDecrement, price, name, id }) => {
+const MintCard = ({ incr, decr, name, counter }) => {
   return (
     <div className="mint-card">
       <div className="mint-card__heading">{name}</div>
       <div className="mint-card__price">0.001 SOL</div>
       <div className="mint-card__counter-box">
-        <a
-          className="mint-card__counter-box--inc"
-          href="#"
-          onClick={handleIncrement}
-        >
+        <a className="mint-card__counter-box--inc" href="#" onClick={incr}>
           +
         </a>
-        <span className="mint-card__counter-box--count">{price}</span>
-        <a
-          className="mint-card__counter-box--dec"
-          href="#"
-          onClick={handleDecrement}
-        >
+        <span className="mint-card__counter-box--count">{counter}</span>
+        <a className="mint-card__counter-box--dec" href="#" onClick={decr}>
           -
         </a>
       </div>
--- src/pages/mint.jsx
+++ src/pages/mint.jsx
@@ -4,7 +4,13 @@ import MintCard from "../components/mintCard";
 import "../css/styles.css";
 
 const Mint = () => {
-  const [price, setPrice] = useState(0);
+  const [smallCtr, setSmallCtr] = useState(0);
+  const [mediumCtr, setMediumCtr] = useState(0);
+  const [largeCtr, setLargeCtr] = useState(0);
+  const [ultraCtr, setUltraCtr] = useState(0);
+
+  const [hidden, setHidden] = useState(false);
+
   const [totalPrice, setTotalPrice] = useState(0);
   const [mintData, setMintData] = useState([]);
 
@@ -12,12 +18,44 @@ const Mint = () => {
     setMintData(getMint());
   }, []);
 
-  const handleIncrement = () => {
-    setPrice((current) => price + 1);
+  const mintNow = () => {
+    setHidden((current) => !hidden);
   };
 
-  const handleDecrement = () => {
-    setPrice((current) => price - 1);
+  const handleCross = () => {
+    setHidden((current) => !hidden);
+  };
+
+  const smallIncrement = () => {
+    setSmallCtr((current) => smallCtr + 1);
+  };
+
+  const smallDecrement = () => {
+    setSmallCtr((current) => smallCtr - 1);
+  };
+
+  const mediumIncrement = () => {
+    setMediumCtr((current) => mediumCtr + 1);
+  };
+
+  const mediumDecrement = () => {
+    setMediumCtr((current) => mediumCtr - 1);
+  };
+
+  const largeIncrement = () => {
+    setLargeCtr((current) => largeCtr + 1);
+  };
+
+  const largeDecrement = () => {
+    setLargeCtr((current) => largeCtr - 1);
+  };
+
+  const ultraIncrement = () => {
+    setUltraCtr((current) => ultraCtr + 1);
+  };
+
+  const ultraDecrement = () => {
+    setUltraCtr((current) => ultraCtr - 1);
   };
 
   return (
@@ -25,36 +63,61 @@ const Mint = () => {
       <div className="mint-container">
         <h1 className="mint-container__heading">Max Mint per Transaction: 5</h1>
         <div className="mint-card-container">
-          {mintData.map((mint) => (
-            <div key={mint.id}>
-              <MintCard
-                {...mint}
-                price={price}
-                handleIncrement={handleIncrement}
-                handleDecrement={handleDecrement}
-              />
-            </div>
-          ))}
+          <MintCard
+            name="Small"
+            counter={smallCtr}
+            incr={smallIncrement}
+            decr={smallDecrement}
+          />
+          <MintCard
+            name="Medium"
+            counter={mediumCtr}
+            incr={mediumIncrement}
+            decr={mediumDecrement}
+          />
+          <MintCard
+            name="Large"
+            counter={largeCtr}
+            incr={largeIncrement}
+            decr={largeDecrement}
+          />
+          <MintCard
+            name="Ultra"
+            counter={ultraCtr}
+            incr={ultraIncrement}
+            decr={ultraDecrement}
+          />
         </div>
         <div className="mint-container__pricebox">
           <span className="mint-container__pricebox__pricename">
             Total price:
           </span>
-          <input
-            className="mint-container__pricebox__input"
-            type="text"
-            name="price"
-            value={totalPrice}
-          />
+          <div className="mint-container__pricebox__input">{totalPrice}</div>
         </div>
         <span className="seperator"></span>
 
         <div className=" u-mt">
-          <a href="#" className="btn btn-oval">
+          <a href="#" className="btn btn-oval" onClick={mintNow}>
             Mint now
           </a>
         </div>
       </div>
+      <div
+        // className="mint-modal hidden"
+        className={hidden ? "mint-modal" : "hidden"}
+      >
+        <span className="mint-modal__cross" onClick={handleCross}>
+          &times;
+        </span>
+        <h1 className="mint-modal__heading">Minting in Process</h1>
+        <a href="#" className="btn btn-oval">
+          View Transaction
+        </a>
+      </div>
+      <div
+        // className="overlay hidden"
+        className={hidden ? "overlay" : "hidden"}
+      ></div>
     </main>
   );
 };
